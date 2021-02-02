6a1f3d07eb8649c5b4cd9e5722300288edbf9ab5
suzygiv <suzannegivnish@gmail.com>
2021-02-02 11:24:17 -0500

    added package.json to client folder

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ if (process.env.NODE_ENV === "production") {
 // Define API routes here
 app.use(routes)
 
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
 
